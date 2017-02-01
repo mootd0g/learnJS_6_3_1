0f688da51e7c42b3e8722316ef7ad0a121ded62c
Andrey Shagov <writeletterto@gmail.com>
2017-02-01 22:17:08 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict'
+
+function Calculator() {
+
+	this.operations = {
+		'+' : function(a, b) {
+			return ( a + b );
+		},
+		'-' : function(a, b) {
+			return ( a * b );
+		}
+	};
+
+	this.addMethod = function(char, func) {
+		this.operations[char] = func;
+	};
+
+	this.calculate = function(str) {
+		var operationAsArray = str.split(' ');
+
+		var firstOperand = +operationAsArray[0];
+		var secondOperand = +operationAsArray[2];
+		var operator = operationAsArray[1];
+
+		return this.operations[operator](firstOperand, secondOperand); 
+	}
+}
+
+/*
+var calc1 = new Calculator();
+console.log( calc1.calculate('10 + 11') );
+
+powerCalc.addMethod("*", function(a, b) {
+  return a * b;
+});
+*/
